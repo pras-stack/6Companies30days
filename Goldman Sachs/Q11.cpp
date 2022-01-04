3ae0eb353d212c80e2e08e70861058d9cc9bd7fc
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-01-04 13:39:57 +0530

    Update Q11.cpp

--- Goldman Sachs/Q11.cpp
+++ Goldman Sachs/Q11.cpp
@@ -3,49 +3,26 @@ Question : https://practice.geeksforgeeks.org/problems/find-missing-and-repeatin
 Solution:
 class Solution{
 public:
-    int *findTwoElement(int *arr, int n) {
-        int XOR = 0;
-    for (int i = 0; i < n; ++i)
-      XOR ^= ((i + 1) ^ arr[i]);
-
-    int setbit_no = XOR & ~(XOR - 1);
-    int bucket1 = 0, bucket2 = 0;
-
-    for (int i = 0; i < n; ++i)
-    {
-      if ((arr[i] & setbit_no))
-        bucket1 ^= arr[i];
-      else
-        bucket2 ^= arr[i];
-
-      if (((i + 1) & setbit_no))
-        bucket1 ^= (i + 1);
-      else
-        bucket2 ^= (i + 1);
+   int *findTwoElement(int *arr, int n) 
+   {
+        // code here
+  
+        int *ans = new int [2];
+        for(int i=0;i<n;i++)
+        {
+            if(arr[abs(arr[i])-1]>0)
+                arr[abs(arr[i])-1] = -arr[abs(arr[i])-1];
+            else
+            ans[0]=abs(arr[i]);
+            
+        }
+        for( int i=0;i<n;i++)
+        {
+            if(arr[i]>0)
+            ans[1]=i+1;
+        }
+        
+        return ans;
     }
-
-    int *ans = new int[2];
-
-    int flag = 0;
-    for (int i = 0; i < n; ++i)
-    {
-      if (arr[i] == bucket1)
-      {
-        flag = 1;
-        break;
-      }
-    }
-
-    if (flag)
-    {
-      ans[0] = bucket1;
-      ans[1] = bucket2;
-    }
-    else
-    {
-      ans[1] = bucket1;
-      ans[0] = bucket2;
-    }
-    return ans;
     }
 };
