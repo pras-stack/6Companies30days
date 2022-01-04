f58d452ced1d0a34899759da409ecec363ba1b13
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-01-04 13:42:03 +0530

    Update Q12.cpp

--- Goldman Sachs/Q12.cpp
+++ Goldman Sachs/Q12.cpp
@@ -1,3 +1,8 @@
-Question : https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1/
+Question :  Find total number of Squares in a N*N chessboard
   
 Solution:
+int countSquares(int n)
+{
+    
+    return (n * (n + 1) / 2) * (2*n + 1) / 3;
+}
