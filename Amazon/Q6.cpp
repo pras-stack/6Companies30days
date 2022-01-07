c976eaab61ff6faf2b48d6527b5ff53bea9cf833
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-01-07 13:17:34 +0530

    Create Q6.cpp

---
+++ Amazon/Q6.cpp
@@ -0,0 +1,26 @@
+Question : https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1
+
+Solution : 
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+     vector <int> max_of_subarrays(int *nums, int n, int k)
+    {
+        // your code here
+        vector<int> ans;
+        deque<int> q;
+        for(int i=0;i<n;i++){
+            while(!q.empty() && i-q.front()>=k){
+                q.pop_front();
+            }
+            while(!q.empty() && nums[q.back()]<nums[i]){
+                q.pop_back();
+            }
+            q.push_back(i);
+            if(i>=k-1)
+                ans.push_back(nums[q.front()]);
+        }
+        return ans;
+    }
+};
