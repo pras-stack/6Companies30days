fc6f6c5d9f902f2600b3d6a0d3bb614230a543cc
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-01-10 11:41:43 +0530

    Update Q10.cpp

--- Amazon/Q10.cpp
+++ Amazon/Q10.cpp
@@ -1,4 +1,30 @@
 Question : https://practice.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1
 Solution :
+class Solution{
+public:	
+
+	void matchPairs(char nuts[], char bolts[], int n) 
+	{
+	    string str = "! # $ % & * @ ^ ~." ;
+	    int p=0;
+	    for(int i=0; i<10; i++)
+	    {
+	        for(int j=0; j<n; j++)
+	        {
+	            if(nuts[j]==str[i])
+	            {
+	                swap(nuts[j],nuts[p]);
+	                bolts[p]=nuts[p];
+	                p++;
+	                if(p>n-1)
+	                {
+	                    break;
+	                }
+	            }
+	        }
+	    }
+	}
+
+};
 
 
