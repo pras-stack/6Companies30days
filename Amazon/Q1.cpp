04bdc26a5242ec9c162249b55998527fc61e3250
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-01-11 12:05:55 +0530

    Update Q1.cpp

--- Amazon/Q1.cpp
+++ Amazon/Q1.cpp
@@ -1,3 +1,21 @@
 Question : https://practice.geeksforgeeks.org/problems/maximum-profit4657/1
   
   Solution:
+class Solution {
+  public:
+    int maxProfit(int K, int N, int A[]) 
+    {
+        int dp[K+1][N];
+       memset(dp,0,sizeof(dp));
+       for(int i=1;i<=K;i++)
+       {
+           int mx=INT_MIN;
+           for(int j=1;j<N;j++)
+           {
+               mx=max(mx,dp[i-1][j-1]-A[j-1]);
+               dp[i][j]=max(dp[i][j-1],mx+A[j]);
+           }
+       }
+       return dp[K][N-1];
+    }
+};
