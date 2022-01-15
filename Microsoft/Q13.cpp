a47f46efccc6702bdc6291e1f5d2b118c5677dc0
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-01-16 00:10:03 +0530

    Create Q13.cpp

---
+++ Microsoft/Q13.cpp
@@ -0,0 +1,22 @@
+Question : https://practice.geeksforgeeks.org/problems/bridge-edge-in-graph/1
+Solutin :  public:
+    //Function to find if the given edge is a bridge in graph.
+    int vis[100001];
+    int dfs(int ind, vector<int> adj[],int c,int d){
+        vis[ind]=1;
+        for(auto it:adj[ind]){
+            if(ind ==c && it==d) continue;
+            if(!vis[it])
+                dfs(it,adj,c,d);
+        }
+        return 0;
+    }
+    int isBridge(int V, vector<int> adj[], int c, int d) 
+    {
+        // Code here
+        for(int i=0;i<V;i++){
+            vis[i]=0;
+        }
+        dfs(c,adj,c,d);
+        return (!vis[d]);
+    }
