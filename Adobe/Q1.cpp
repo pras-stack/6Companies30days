c03a4a476ac34ad648bf6cbbc022eae1da4523d0
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-01-19 13:03:01 +0530

    Update Q1.cpp

--- Adobe/Q1.cpp
+++ Adobe/Q1.cpp
@@ -1,1 +1,38 @@
+Question : https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1#
+
+Solution : 
+
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+     vector<int> subarraySum(int a[], int n, long long s)
+    {
+        // Your code here
+        
+     int l = 0, h = n - 1, sum = 0, i = 0;
+        while(l <= h)
+        {
+            sum+=a[i];
+            if(sum == s)
+            {
+                return {l+1,i+1};
+            }
+            if(sum > s)
+            {
+                while(sum > s)
+                {
+                    sum-=a[l];
+                    l++;
+                    if(sum == s)
+                    {
+                        return {l+1,i+1};
+                    }
+                }
+            }
+            i++;
+        }
+        return {-1};
+    }
+};
 
