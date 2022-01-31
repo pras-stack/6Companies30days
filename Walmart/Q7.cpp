80399e3a6749621ebc29927072c634fb5b8a2d96
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-02-01 00:31:49 +0530

    Create Q7.cpp

---
+++ Walmart/Q7.cpp
@@ -0,0 +1,30 @@
+Question : https://practice.geeksforgeeks.org/problems/sorted-subsequence-of-size-3/1/
+Solution : 
+class Solution{
+  public:
+    vector<int> find3Numbers(vector<int> arr, int N) 
+    {
+           int large[N+1]={0}, small[N+1]={0};
+        int mini = INT_MAX, maxi = INT_MIN;
+        for(int i=0;i<N-1;i++)
+        {
+            mini = min(mini,arr[i]);
+            small[i+1]=mini;
+        }
+        for(int i=N-1;i>0;i--)
+        {
+            maxi =max(maxi,arr[i]);
+            large[i-1]=maxi;
+        }
+        for(int i=1;i<N-1;i++)
+        {
+            if(arr[i]>small[i] && arr[i]<large[i])
+                return 
+                {
+                    small[i],arr[i],large[i]
+                    
+                };
+        }
+        return {};
+    }
+};
