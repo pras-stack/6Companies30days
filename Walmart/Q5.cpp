c9eb69d0585af6c85b56057d647c642777c5ac9a
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-02-01 00:25:32 +0530

    Create Q5.cpp

---
+++ Walmart/Q5.cpp
@@ -0,0 +1,26 @@
+Question : https://practice.geeksforgeeks.org/problems/transform-to-sum-tree/1/
+Solution : 
+class Solution {
+  public:
+  
+    int helper(Node *root){
+    if(root==NULL){
+        return 0;
+    }
+    if(root->left==NULL and root->right==NULL){
+        int t = root->data;
+        root->data=0;
+        return t;
+    }
+    int ls = helper(root->left);
+    int rs = helper(root->right);
+    int tmp = root->data;
+    root->data = ls+rs;
+    return tmp+ls+rs;
+}
+
+void toSumTree(Node *node)
+{
+    helper(node);
+}
+};
